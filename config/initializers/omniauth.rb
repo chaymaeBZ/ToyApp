5a5a4448fd6ad4bc34d6266ec1d833091db85a4b
BAZZAOUI chaymae <bazzaouichaymae@MacBook-Pro-de-BAZZAOUI.local>
2016-12-07 22:01:07 +0000

    bug fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['uUOlBf7dM2retzJMNEqTgQekM'],
-            ENV['FyvZXC6Rove2MOyU0MGNWra3yKn7CRjpJ8qDnEC4YxKYJbCQDz']
+  provider :twitter, ENV['uUOlBf7dM2retzJMNEqTgQekM'], ENV['FyvZXC6Rove2MOyU0MGNWra3yKn7CRjpJ8qDnEC4YxKYJbCQDz']
 end
